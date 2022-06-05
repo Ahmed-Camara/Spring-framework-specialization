3fe9844ab6a037017f492e7dfcc1fb02158139a7
Ahmed-Camara <camarahmed123@gmail.com>
2022-06-05 15:46:46 +0000

    Request Parameters

--- Spring MVC, Spring Boot and Rest Controllers/StudentWork/setup/Lab2/student/src/main/java/com/student/controller/StudentController.java
+++ Spring MVC, Spring Boot and Rest Controllers/StudentWork/setup/Lab2/student/src/main/java/com/student/controller/StudentController.java
@@ -1,12 +1,16 @@
 package com.student.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.StudentProperties;
@@ -26,14 +30,25 @@ public class StudentController {
 	@Inject private StudentProperties studentProperties;
 	
 	@GetMapping("msg")
-	public String getMessage() {
+	public String getMessage(@RequestHeader("user-agent")String userAgent) {
 		
-		return studentProperties.getGreeting();
+		return studentProperties.getGreeting()+" "+userAgent;
 	}
 	
 	@GetMapping
 	public Collection<Student> getAll(){
 		return studentService.getAllStudents();
+	}
+	
+	@GetMapping("/{id}")
+	public Student getStudent(@PathVariable("id") long id){
+		
+		return studentService.get(id);
+	}
+	
+	@GetMapping("/single")
+	public Student getSingleStudent(@RequestParam("id") Optional<Long> optional) {
+		return studentService.get(optional.orElse(1l));
 	}
 	
 	@GetMapping("/test")
